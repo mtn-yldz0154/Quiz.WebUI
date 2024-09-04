844e8933293c5f4d9aa2d7162cb391cfa79f2c20
Metin Yıldız <75701995+mtn-yldz0154@users.noreply.github.com>
2024-09-04 22:27:59 +0300

    dsfsdf

--- Quiz.WebUI/Controllers/QuizController.cs
+++ Quiz.WebUI/Controllers/QuizController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.WebUI.Context;
 
 namespace Quiz.WebUI.Controllers
 {
     public class QuizController : Controller
     {
+        private QuizContext _quizContext;
+
+        public QuizController(QuizContext quizContext)
+        {
+            _quizContext = quizContext;
+        }
         public IActionResult Index()
         {
+       
+            var oturum = _quizContext.Oturums
+                .Where(i => i.Date > DateTime.Now)
+                .OrderBy(i => i.Date)
+                .FirstOrDefault();
+            if(oturum == null)
+            {
+                ViewBag.State = 0;
+            }
+            else
+            {
+                ViewBag.State = 1;
+
+                ViewBag.Oturum = oturum;
+            }
+
             return View();
         }
     }
